b0727cc2d9c4f306f33a60a58926808ecc3a0ea3
Mayur Bhatia <23645213+myrse7en@users.noreply.github.com>
2018-12-12 14:01:45 -0500

    Initial commit

---
+++ src/components/navbar/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './index.css';
+import { NavLink } from 'react-router-dom'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <NavLink className="navbar-brand" to='/'>Weather App</NavLink>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <NavLink className="nav-link" to='/'>Home <span className="sr-only">(current)</span></NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to='/checkout'>Checkout</NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
---
+++ src/views/checkout/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Checkout extends Component {
+  render() {
+    return (
+      <div className="Checkout">
+        <h1>Checkout</h1>
+      </div>
+    );
+  }
+}
+
+export default Checkout;
